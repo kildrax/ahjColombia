35dc68969b8c54c19daeddb7c158391ea03cefbb
kildrax <jcbest15@hotmail.com>
2021-06-05 18:33:17 -0500

    ajustes pequeños a estilos del sitio y se agrega el botón whatsapp flotante

--- wp-content/themes/ahj/footer.php
+++ wp-content/themes/ahj/footer.php
@@ -1,7 +1,7 @@
 <footer class="footer--bg">
     <div class="footer content">
         <div class="footer__content">
-            <?php wp_nav_menu( array('theme_location' => 'footer_menu', 'menu_class' => 'footer__content-links-ul') ); ?>
+            <?php wp_nav_menu(array('theme_location' => 'footer_menu', 'menu_class' => 'footer__content-links-ul')); ?>
         </div>
         <div class="footer__content">
             <span class="footer__content_txt--style">Dirección: <br /><?php echo get_option('direccion_ahj'); ?></span>
@@ -11,8 +11,7 @@
         </div>
         <div class="footer__content">
             <span class="footer__content_txt--style">Correo: <br />
-                <a href="mailto:<?php echo get_option('correo_ahj'); ?>"
-                    class="footer__content_txt--style"><?php echo get_option('correo_ahj'); ?></a>
+                <a href="mailto:<?php echo get_option('correo_ahj'); ?>" class="footer__content_txt--style"><?php echo get_option('correo_ahj'); ?></a>
             </span>
             <span class="footer__content_txt--style">Celular: <br />
                 <a href="tel:<?php echo get_option('celular_ahj'); ?>" class="footer__content_txt--style"><?php echo get_option('celular_ahj'); ?></a>
@@ -23,22 +22,23 @@
         </div>
         <div class="footer__content">
             <a href="<?php echo get_option('fb_link'); ?>" target="_blank" class="footer__content_social_network">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/facebook_icon.png"
-                    class="footer__content_social_network-img" alt="Facebook">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/facebook_icon.png" class="footer__content_social_network-img" alt="Facebook">
             </a>
             <a href="<?php echo get_option('insta_link'); ?>" target="_blank" class="footer__content_social_network">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/instagram.png"
-                    class="footer__content_social_network-img" alt="Instagram">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/instagram.png" class="footer__content_social_network-img" alt="Instagram">
             </a>
-            <a href="https://api.whatsapp.com/send?phone=<?php echo get_option('whatsapp_number'); ?>" target="_blank"
-                class="footer__content_social_network">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/whatsapp.png"
-                    class="footer__content_social_network-img" alt="Whatsapp">
+            <a href="https://api.whatsapp.com/send?phone=<?php echo get_option('whatsapp_number'); ?>" target="_blank" class="footer__content_social_network">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/whatsapp.png" class="footer__content_social_network-img" alt="Whatsapp">
             </a>
         </div>
     </div>
 </footer>
-
+<div class="floating__whatsapp">
+    <a href="https://api.whatsapp.com/send?phone=<?php echo get_option('whatsapp_number'); ?>" target="_blank" class="floating__whatsapp_link">
+        <img src="<?php echo get_template_directory_uri(); ?>/assets/img/icons/whatsapp_icon.png" alt="Whatsapp AHJ Colombia">
+        <span>Chatea con nosotros</span>
+    </a>
+</div>
 <?php wp_footer(); ?>
 
 </body>
